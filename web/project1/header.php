bbf99537ad32f840f85d5a6b7acbc2a17ea85db8
TravisCon <confer.travis@gmail.com>
2017-05-20 03:31:50 -0600

    Some more items finished

--- web/project1/header.php
+++ web/project1/header.php
@@ -1,15 +1,17 @@
 <script src="script.js"></script>
 <div class="row" id="header">
   <div class="col-md-12">
-    <h2 id="title">Rookie Cook</h2>
-    <h2 id="subtitle">
-      <?php 
-      echo $currentPage;
-      spl_autoload_register(function($className){
-        require_once $className.".php"; 
-      });
-      ?>
+    <a href="main.php">
+      <h2 id="title">Rookie Cook</h2>
+      <h2 id="subtitle">
+        <?php 
+        echo $currentPage;
+        spl_autoload_register(function($className){
+          require_once $className.".php"; 
+        });
+        ?>
 
-    </h2>
+      </h2>
+    </a>
   </div>
 </div>
